0b22f2b8a058356c32948afab12c4d5a5904929d
Tim Masliuchenko <timfjord@hey.com>
2021-07-21 17:59:06 +0100

    Implementation

---
+++ expand_selection_to_comments.py
@@ -0,0 +1,58 @@
+import sublime
+import sublime_plugin
+import re
+
+
+class ExpandSelectionToCommentsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # enable soft-undo
+        sublime.set_timeout(lambda:
+            self.view.run_command('expand_selection_to_comments_atomic'),
+            0
+        )
+
+class ExpandSelectionToCommentsAtomicCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view_range = range(0, self.view.size() + 1)
+
+        for region in self.view.sel():
+            begin = region.begin()
+            end = region.end()
+            region_begin = self.find_region_boundary(begin, False)
+            region_end = self.find_region_boundary(end, True)
+
+            if begin != region_begin or end != region_end:
+                self.view.sel().add(sublime.Region(region_begin, region_end))
+
+    def find_region_boundary(self, start, forward=True):
+        res = cur = start
+
+        while self.is_withint_comment(cur):
+            if self.is_comment(cur) or self.is_last_comment_char(cur): res = cur
+
+            if forward: cur += 1
+            else: cur -= 1
+
+        return res
+
+    def is_comment(self, point):
+        return self.view.match_selector(point, 'comment')
+
+    def is_last_comment_char(self, point):
+        """
+        Check whether the point is on the last char in the line and there is a comment char before
+        Mostly to cover this case:
+
+        # comment goes here
+                          ^ -> cursor is here and there is no trailing newline
+
+        Without this check this character is simple ignored
+        """
+        return (self.view.classify(point) & sublime.CLASS_LINE_END != 0) and self.is_comment(point - 1)
+
+    def is_withint_comment(self, point):
+        if point not in self.view_range: return False
+
+        char = self.view.substr(point)
+
+        return self.is_comment(point) or re.match(r'\s', char) or self.is_last_comment_char(point)
